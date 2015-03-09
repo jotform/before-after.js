d9b2e2de4a73d56a7c05adba2fbfebd608a74d37
tatarjr <ege@alumni.bilkent.edu.tr>
2015-03-09 19:01:55 +0200

    Native touchevent support added
    You can now remove jquery mobile

--- before-after.js
+++ before-after.js
@@ -1,14 +1,17 @@
 function drags(dragElement, resizeElement, container) {
 	
   // Initialize the dragging event on mousedown.
-  dragElement.on('mousedown vmousedown', function(e) {
+  dragElement.on('mousedown touchstart', function(e) {
     
     dragElement.addClass('draggable');
     resizeElement.addClass('resizable');
     
+    // Check if it's a mouse or touch event and pass along the correct value
+    var startX = (e.pageX) ? e.pageX : e.originalEvent.touches[0].pageX;
+    
     // Get the initial position
     var dragWidth = dragElement.outerWidth(),
-        posX = dragElement.offset().left + dragWidth - e.pageX,
+        posX = dragElement.offset().left + dragWidth - startX,
         containerOffset = container.offset().left,
         containerWidth = container.outerWidth();
  
@@ -17,9 +20,12 @@ function drags(dragElement, resizeElement, container) {
     maxLeft = containerOffset + containerWidth - dragWidth - 10;
     
     // Calculate the dragging distance on mousemove.
-    dragElement.parents().on("mousemove", function(e) {
-
-      leftValue = e.pageX + posX - dragWidth;
+    dragElement.parents().on("mousemove touchmove", function(e) {
+			
+			// Check if it's a mouse or touch event and pass along the correct value
+    	var moveX = (e.pageX) ? e.pageX : e.originalEvent.touches[0].pageX;
+      
+      leftValue = moveX + posX - dragWidth;
       
       // Prevent going off limits
       if ( leftValue < minLeft) {
@@ -33,17 +39,17 @@ function drags(dragElement, resizeElement, container) {
 			
       // Set the new values for the slider and the handle. 
       // Bind mouseup events to stop dragging.
-      $('.draggable').css('left', widthValue).on('mouseup vmouseup', function () {
+      $('.draggable').css('left', widthValue).on('mouseup touchend touchcancel', function () {
         $(this).removeClass('draggable');
         resizeElement.removeClass('resizable');
       });
       $('.resizable').css('width', widthValue);
-    }).on('mouseup vmouseup', function(){
+    }).on('mouseup touchend touchcancel', function(){
       dragElement.removeClass('draggable');
       resizeElement.removeClass('resizable');
     });
     e.preventDefault();
-  }).on('mouseup vmouseup', function(e){
+  }).on('mouseup touchend touchcancel', function(e){
     dragElement.removeClass('draggable');
     resizeElement.removeClass('resizable');
   });
@@ -62,6 +68,7 @@ $(document).ready(function(){
 });
 
 // Update sliders on resize. 
+// Because we all do this: i.imgur.com/YkbaV.gif
 $(window).resize(function(){
   $('.ba-slider').each(function(){
     var cur = $(this);
